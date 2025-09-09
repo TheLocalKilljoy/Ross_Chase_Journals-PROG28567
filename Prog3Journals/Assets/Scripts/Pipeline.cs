5899e5d545b06d07a532eb99d84725767f03c17e
Chase Ross <chaseross002@gmail.com>
2025-09-08 23:34:25 -0400

    Task 3b attempted, but not working

--- Prog3Journals/Assets/Scripts/Pipeline.cs
+++ Prog3Journals/Assets/Scripts/Pipeline.cs
@@ -1,18 +1,34 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Pipeline : MonoBehaviour
 {
-    // Update is called once per frame
+    Vector2 pos = new Vector2();
+
+    Vector2 newPos = new Vector2();
+
     void Update()
     {
+        Vector2 mousePos = Input.mousePosition;
+
         if (Input.GetMouseButtonDown(0))
         {
-
-            Vector2 mousePos = Input.mousePosition;
-            Vector2 pos = new Vector2();
             pos = Camera.main.ScreenToWorldPoint(mousePos);
 
             Debug.Log(pos);
+
+            for (int i = 0; i < 10; i++) { }
+            
+            if (Input.GetMouseButtonDown(0) == true) 
+            {
+                newPos = Camera.main.ScreenToWorldPoint(mousePos);
+
+                Debug.DrawLine(pos, newPos);
+
+                Debug.Log(pos);
+
+            }
         }
     }
 }
