7a9345196c84f9b1aa2d9d5b891a7a18bee3f4bf
Chase Ross <chaseross002@gmail.com>
2025-09-19 22:39:47 -0400

    task 3 completed

--- Prog3Journals/Assets/Scripts/Controllers/Player.cs
+++ Prog3Journals/Assets/Scripts/Controllers/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
@@ -16,6 +17,8 @@ public class Player : MonoBehaviour
     public int numberOfTrailBombs;
 
     Vector2 spawnCorner = new Vector2(0,1);
+
+    public float warpRatio = 0.2f;
 
     // Update is called once per frame
     void Update()
@@ -34,6 +37,11 @@ public class Player : MonoBehaviour
         {
             SpawnBombAtCorners(transform.position, Vector2.one);
         }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            WarpPlayer(enemyTransform, warpRatio);
+        }
     }
 
     void SpawnBombAtOffset(Vector3 inOffset)
@@ -46,7 +54,6 @@ public class Player : MonoBehaviour
     {
         Vector3 spawn = transform.position + bombOffset;
 
-
         for (int i = 0; i < inNumberOfBombs; i++)
             Instantiate(bombPrefab, spawn, Quaternion.identity);
     }
@@ -55,41 +62,49 @@ public class Player : MonoBehaviour
     {
         float halfWidth = size.x / 2;
         float halfHeight = size.y / 2;
+        int corner = UnityEngine.Random.Range(1, 5);
 
         Vector2 topLeft = position + new Vector2(-halfWidth, halfHeight);
         Vector2 topRight = topLeft + new Vector2(size.x, 0);
         Vector2 bottomRight = topRight + new Vector2(0, -size.y);
         Vector2 bottomLeft = bottomRight + new Vector2(-size.x, 0);
 
-        int corner = UnityEngine.Random.Range(1, 5);
-
         if (corner == 1) 
         {
             spawnCorner = topRight;
             Instantiate(bombPrefab, spawnCorner, Quaternion.identity);
             Debug.Log("1");
-
         }
         if (corner == 2) 
         {
             spawnCorner = topLeft;
             Instantiate(bombPrefab, spawnCorner, Quaternion.identity);
             Debug.Log("2");
-
         }
         if (corner == 3) 
         {
             spawnCorner = bottomLeft;
             Instantiate(bombPrefab, spawnCorner, Quaternion.identity);
             Debug.Log("3");
-
         }
         if (corner == 4) 
         {
             spawnCorner = bottomRight;
             Instantiate(bombPrefab, spawnCorner, Quaternion.identity);
             Debug.Log("4");
-
         }
+    }
+
+    public void WarpPlayer(Transform target, float ratio)
+    {
+
+        if (warpRatio <= 1) 
+        {
+            ratio = warpRatio;
+
+            transform.position = Vector2.Lerp(transform.position, target.position, ratio);
+        }
+
+        Debug.Log("method works");
     }
 }
